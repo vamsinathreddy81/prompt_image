187470ecb199ffd6293702d4a6ed84351f241901
vamsinathreddy81 <126274660+vamsinathreddy81@users.noreply.github.com>
2023-11-24 09:43:12 +0530

    Update dalle.py

--- dalle.py
+++ dalle.py
@@ -1,53 +1,109 @@
-import os
 import openai
 import webbrowser
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+from urllib.parse import parse_qs
 
-
-
-# Get the OpenAI API key from the .env file
-api_key = "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"
-
-if not api_key:
-    raise ValueError("Api key not found in .env file")
-
+api_key = "ai key"
 openai.api_key = api_key
 
-user_prompt = input("Write A Prompt to Generate Image: ")
-
-response = openai.Image.create(
-    prompt=user_prompt,
-    n=1,
-    size="1024x1024"
-)
-
-image_url = response['data'][0]['url']
-
-# create an HTML template with the image
-html_template = f"""
+html_input_form = """
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Image Display</title>
+    <title>Image Generation</title>
+    <style>
+        body {
+            background-image: url('OIP.jpeg');
+            /* Replace 'background.jpg' with your image file */
+            background-size: cover;
+            height:100vh;
+            color: white;
+            font-family: 'Arial', sans-serif;
+        }
+
+        h1 {
+            text-align: center;
+        }
+
+        form {
+            width: 50%;
+            margin: 0 auto;
+            background: rgba(0, 0, 0, 0.7);
+            padding: 20px;
+            border-radius: 10px;
+        }
+
+        label, input, button {
+            display: block;
+            margin: 10px 0;
+        }
+
+        input {
+            width: 100%;
+            padding: 8px;
+        }
+
+        button {
+            background-color: #4CAF50;
+            color: white;
+            padding: 10px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+    </style>
 </head>
 <body>
-    <h1>Generated Image - ByteWebster</h1>
-    <img src="{image_url}" alt="Generated Image" width="500">
+    <h1>Generate Image</h1>
+    <form action="/generate_image" method="post">
+        <label for="prompt">Enter Prompt:</label>
+        <input type="text" id="prompt" name="prompt" required>
+        <button type="submit">Generate Image</button>
+    </form>
 </body>
 </html>
 """
 
-output_folder = "dalle-images"
-os.makedirs(output_folder, exist_ok=True)
+class RequestHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(html_input_form.encode())
+        else:
+            super().do_GET()
 
-# Define the file path for the HTML File
-file_path = os.path.join(output_folder, "image_display.html")
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length).decode('utf-8')
+        user_prompt = parse_qs(post_data)['prompt'][0]
 
-with open(file_path, "w") as file:
-    file.write(html_template)
-    
-print(f"Image URL: {image_url}")
-print(f"HTML template saved to '{file_path}'")
+        response = openai.Image.create(prompt=user_prompt, n=1, size="1024x1024")
+        image_url = response['data'][0]['url']
 
-webbrowser.open(file_path)
+        html_template = f"""
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Generated Image</title>
+         
+        </head>
+        <body style="display:flex;align-items:center;justify-content:center;">
+            <h1 style="font-weight:20px;">Generated Image   :</h1>
+            <img style="background:black;" src="{image_url}" alt="Generated Image" width="500">
+        </body>
+        </html>
+        """
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(html_template.encode())
+
+webbrowser.open("http://localhost:8000")
+HTTPServer(('', 8000), RequestHandler).serve_forever()
